4f3e11b9fa5feaf17f56d45c5e5d17af9e4bdd95
sakthi singh <sakthisingh18@gmail.com>
2026-02-23 23:08:49 +0530

    include the skills_db.py

---
+++ skills_db.py
@@ -0,0 +1,19 @@
+# skills_db.py
+
+SKILL_DB = [
+    # Programming
+    "python", "java", "c", "c++", "sql", "dart",
+
+    # Web
+    "html", "css", "javascript", "react", "node",
+
+    # Data
+    "machine learning", "deep learning", "pandas",
+    "numpy", "tensorflow", "pytorch", "data analysis",
+
+    # Mobile
+    "flutter", "android", "ios",
+
+    # Tools
+    "git", "docker", "aws", "excel", "power bi"
+]
